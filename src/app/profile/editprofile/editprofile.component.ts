3d3a193af617f103c8825a09f2888cea5d8afd17
anthonyxavier64 <e0406854@u.nus.edu>
2021-04-03 01:30:01 +0800

    edit profile done

--- src/app/profile/editprofile/editprofile.component.ts
+++ src/app/profile/editprofile/editprofile.component.ts
@@ -1,29 +1,56 @@
-import { Input } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { StudentWrapper } from 'src/app/models/student-wrapper';
+import { SessionService } from 'src/app/services/session.service';
+import { StudentService } from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-editprofile',
   templateUrl: './editprofile.component.html',
   styleUrls: ['./editprofile.component.css'],
+  providers: [MessageService]
 })
 export class EditprofileComponent implements OnInit {
   @Input() currentStudent: StudentWrapper;
+  
+  @Output() childEvent = new EventEmitter<StudentWrapper>();
+
+  editStudent: StudentWrapper;
   startDate: string;
   endDate: string;
   newSkill: string | undefined;
 
   relevantSkills: string[];
 
-  constructor() {
+  constructor(
+    private studentService: StudentService,
+    private sessionService: SessionService,
+    private messageService: MessageService,
+    private router: Router
+  ) {
     this.currentStudent = new StudentWrapper();
     this.startDate = '';
     this.endDate = '';
     this.relevantSkills = new Array();
+    this.editStudent = new StudentWrapper();
   }
 
   ngOnInit(): void {
+    this.editStudent.name = this.currentStudent.name;
+    this.editStudent.availabilityPeriod = this.currentStudent.availabilityPeriod;
+    this.editStudent.biography = this.currentStudent.biography;
+    this.editStudent.courseOfStudy = this.currentStudent.courseOfStudy;
+    this.editStudent.educationalInstitute = this.currentStudent.educationalInstitute;
+    this.editStudent.email = this.currentStudent.email;
+    this.editStudent.password = this.currentStudent.password;
+    this.editStudent.projectedGraduationYear = this.currentStudent.projectedGraduationYear;
+    this.editStudent.relevantSkills = this.currentStudent.relevantSkills;
+    this.editStudent.studentId = this.currentStudent.studentId;
+    this.editStudent.yearOfStudy = this.currentStudent.yearOfStudy;
+
     if (this.currentStudent.availabilityPeriod !== undefined) {
       this.startDate = this.currentStudent.availabilityPeriod[0];
       this.endDate = this.currentStudent.availabilityPeriod[1];
@@ -33,8 +60,31 @@ export class EditprofileComponent implements OnInit {
     }
   }
 
-  edit(ngForm: NgForm) { 
+  edit(ngForm: NgForm) {
+    if (this.editStudent) {
+      this.editStudent.relevantSkills = this.relevantSkills;
 
+      if (this.editStudent.availabilityPeriod) {
+        this.editStudent.availabilityPeriod[0] = this.startDate;
+        this.editStudent.availabilityPeriod[1] = this.endDate;
+      }
+    }
+
+    this.studentService.editStudentDetails(this.editStudent).subscribe(
+      (response) => {
+        this.sessionService.setCurrentStudent(response);
+        this.messageService.add({
+          severity:'success', summary: 'Account edited succesfully!'
+        });
+        this.childEvent.emit(response);
+        this.router.navigate(["/viewProfile"]);
+      },
+      (error) => {
+        this.messageService.add({
+          severity:'error', summary:"Error", detail:'Unable to edit account'
+        });
+      }
+    );
   }
 
   addSkill() {
--- src/app/profile/viewprofile/viewprofile.component.ts
+++ src/app/profile/viewprofile/viewprofile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -65,5 +65,9 @@ export class ViewprofileComponent {
     ];
 
     this.activeItem = this.items[0];
+  }
+
+  setCurrentStudent(currentStudent: StudentWrapper) { 
+    this.currentStudent = currentStudent;
   }
 }
